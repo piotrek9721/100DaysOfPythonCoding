da00b9831aa7c1c9db3287acfe1ec0149a5eedb3
piotrekkruk <piotrekkruk@icloud.com>
2022-09-02 11:46:25 +0200

    100 Days Of Python

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,4 +27,23 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+print("Welcome to the Game!")
+player = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
+if player >=3 or player < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    comp = random.randint(0, 2)
+    game_images = [rock, paper, scissors]
+    print(f"You chose:{game_images[player]} ")
+    print(f"Computer chose: {game_images[comp]}")
 
+    if player == 0 and comp == 2:
+        print("You win!")
+    if comp == 0 and player == 2:
+        print("you lose!")
+    if comp > player:
+        print("You lose!")
+    if player > comp:
+        print("You win!")
+    if comp == player:
+        print("it is a draw!")
